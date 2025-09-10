eb4850933d33143ffee255f02fafff4f9cdde91e
Quy Le <minhquyle2k6@gmail.com>
2025-09-10 12:01:26 +0300

    feat: add example bag recorder node

---
+++ adas_system/ros_bag_recorder/bag_writer.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+
+import rosbag2_py
+import rclpy
+
+# import ros msgs here
+
+
+
+
+# Create ROS 2 node 
+# this node responmsible for ros bag writer
+# tutorial: https://docs.ros.org/en/rolling/Tutorials/Advanced/Recording-A-Bag-From-Your-Own-Node-Py.html
+
+# create a rOS bag writer
+
+
